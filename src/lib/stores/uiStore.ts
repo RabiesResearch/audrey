74643d55fda89c1886a64c5b67727289ef0b8f29
Chas Nelson <github@chasnelson.co.uk>
2025-05-12 14:39:43 +0100

    feat: make sidebar default to closed

--- src/lib/stores/uiStore.ts
+++ src/lib/stores/uiStore.ts
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 
 // UI state stores
-export const sidebarOpen = writable(true);
+export const sidebarOpen = writable(false);
 export const selectedRegion = writable(null);
 export const selectedDistrict = writable(null);
 export const mapZoomLevel = writable(1);
