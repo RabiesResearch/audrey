726fe7fdf56344f70869d57e4054f791e490ea2c
Chas Nelson <github@chasnelson.co.uk>
2025-05-13 16:19:15 +0100

    feat: minor improvement to expand / collapse UI

---
+++ src/lib/components/tables/expand-button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { ChevronDown, ChevronRight } from "@steeze-ui/heroicons";
+  import { Icon } from "@steeze-ui/svelte-icon";
+
+  type Props = {
+    onclick: () => void;
+    canExpand: boolean;
+    isExpanded: boolean;
+  };
+
+  let { onclick, canExpand, isExpanded }: Props = $props();
+</script>
+
+<button disabled={!canExpand} {onclick}>
+  {#if canExpand}
+    <Icon
+      src={isExpanded ? ChevronDown : ChevronRight}
+      size="1.2em"
+      theme="solid"
+    />
+  {/if}
+</button>
