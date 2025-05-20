a1bd384f00dbcef52ba4ed423049797cbe74b057
Chas Nelson <github@chasnelson.co.uk>
2025-05-20 14:07:16 +0100

    feat: make store use API type for allRegionsAndDistricts

--- src/lib/stores/uiStore.ts
+++ src/lib/stores/uiStore.ts
@@ -1,3 +1,4 @@
+import type { RegionAndDistrict } from "$data/api";
 import { writable } from "svelte/store";
 
 // UI state stores
@@ -5,6 +6,4 @@ export const sidebarOpen = writable(false);
 export const selectedRegion = writable<string | null>(null);
 export const selectedDistrict = writable<string | null>(null);
 export const mapZoomLevel = writable(1);
-export const allRegionsAndDistricts = writable<
-  { region: string; district: string }[]
->([]);
+export const allRegionsAndDistricts = writable<RegionAndDistrict[]>([]);
